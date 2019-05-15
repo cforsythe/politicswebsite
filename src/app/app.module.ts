9277f4f8ac621f1d958a1a99560c1d3821c1180e
cforsythe <cforsythe@csumb.edu>
2019-05-14 19:46:13 -0700

    added css styling and navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuestionComponent } from './question/question.component';
 import { QuizComponent } from './quiz/quiz.component';
+import { LearnComponent } from './learn/learn.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+const appRoutes: Routes = [
+ {path: 'learn', component: LearnComponent},
+ {path: '', component: QuizComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     QuestionComponent,
-    QuizComponent
+    QuizComponent,
+    LearnComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
